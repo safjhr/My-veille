bded2e3624c5e29c4ea61768415d2cee928da39e
safjhr <safiya@jaouahir.Com>
2025-07-06 21:27:43 +0200

    first commit

---
+++ src/main/java/com/example/My_veille/MyVeilleApplication.java
@@ -0,0 +1,13 @@
+package com.example.My_veille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVeilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVeilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/My_veille/MyVeilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My_veille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVeilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
